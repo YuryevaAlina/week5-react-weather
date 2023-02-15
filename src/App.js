41f2b78cd6d59805d8db59db5b76191e52567cd9
Alina Yuryeva <yuryeva.alina@gmail.com>
2023-02-15 12:50:32 +0100

    noopener noreferrer

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <h2>New title </h2>
       <footer>
         This project is created by{" "}
-        <a href="" target="_blank" rel="noreferrer">
+        <a href="" target="_blank" rel="noopener noreferrer">
           Alina Yuryeva{" "}
         </a>{" "}
         and{" "}
         <a
           href="https://github.com/YuryevaAlina/week5-react-weather"
           target="_blank"
-          rel="noreferrer"
+          rel="nnoopener noreferrer"
         >
           open-sourced at GitHub
         </a>
