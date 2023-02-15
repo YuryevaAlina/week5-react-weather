6ae5b583d2ee9913fb0001131f22b48661def833
Alina Yuryeva <yuryeva.alina@gmail.com>
2023-02-15 12:38:41 +0100

    added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Wather app</div>;
+  return (
+    <div className="App">
+      <h1>Wather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="" target="_blank">
+          Alina Yuryeva{" "}
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/YuryevaAlina/week5-react-weather"
+          target="_blank"
+        >
+          open-sourced at GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
