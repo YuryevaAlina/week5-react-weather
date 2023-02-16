b92f7a6daedd02fa07684df4ce0bca822d0b51e1
Alina Yuryeva <yuryeva.alina@gmail.com>
2023-02-16 16:31:33 +0100

    add botstrap and Weather element

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Wather App</h1>
-      <h2>New title </h2>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://www.markus-steinacher.at/sichere-externe-links-durch-target_blank-relnoopener-noreferrer/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Alina Yuryeva{" "}
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://github.com/YuryevaAlina/week5-react-weather"
-          target="_blank"
-          rel="nnoopener noreferrer"
-        >
-          open-sourced at GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Wather App</h1>
+        <Weather />
+        <button className="btn btn-primary">Hello</button>
+
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://www.markus-steinacher.at/sichere-externe-links-durch-target_blank-relnoopener-noreferrer/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Alina Yuryeva{" "}
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/YuryevaAlina/week5-react-weather"
+            target="_blank"
+            rel="nnoopener noreferrer"
+          >
+            open-sourced at GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
