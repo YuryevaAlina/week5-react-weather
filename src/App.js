b1ec78d53db871d390e46d0209172afb0432452d
Alina Yuryeva <yuryeva.alina@gmail.com>
2023-02-17 09:31:24 +0100

    props defaul city

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Tokio" />
 
         <footer>
           This project is created by{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -71,7 +71,7 @@ export default function Weather() {
   } else {
     const apiKey = "e46ad43e1790262fc11a1f8b956dcb24";
     let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric `;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric `;
     axios.get(apiUrl).then(handleResponse);
     return "Loading..";
   }
