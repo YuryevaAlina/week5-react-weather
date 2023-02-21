c4902a9e756b976cf9e1d94e3e42afaee08c5ab3
Alina Yuryeva <yuryeva.alina@gmail.com>
2023-02-21 18:58:08 +0100

    delete fahrenhreeit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function convertToFarenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenhehit");
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemparature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit mt-6">
-          °C |{" "}
-          <a href="/" onClick={convertToFarenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemparature">
-        <span className="temperature">{Math.round(fahrenheit())}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemparature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit mt-6">°C</span>
+    </div>
+  );
 }
